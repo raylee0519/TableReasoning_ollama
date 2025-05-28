ece936dbeeb256c102fd693249a699e230768a30
RayLee <wooo519@naver.com>
2025-05-28 06:22:08 +0000

    Change WikiTQ evaluate file

--- ReAcTable/WikiTQ_evaluate.py
+++ ReAcTable/WikiTQ_evaluate.py
@@ -1,19 +1,13 @@
-import os
-import shutil
+import subprocess
 
-# 설정
-# maxLimit = 5
-local_result_file = f'./result/result_4344.json'
-eval_result_dir = './dataset/WikiTableQuestions/results/'
-eval_result_file = os.path.join(eval_result_dir, f'result_4344.json')
-evaluator_path = './dataset/WikiTableQuestions/evaluator.py'
+# Relative path to evaluator directory
+evaluator_dir = './dataset/WikiTableQuestions'
+evaluator_script = 'evaluator.py'
+# Match the prediction path relative to the evaluator's working directory
+relative_prediction_path = '../../result/result_4344.json'
 
-# 평가용 디렉토리가 없으면 생성
-if not os.path.exists(eval_result_dir):
-    os.makedirs(eval_result_dir)
-
-# 결과 파일 복사 (존재하지 않아도 생성됨)
-shutil.copy(local_result_file, eval_result_file)
-
-# 평가 실행
-os.system(f'cd ./dataset/WikiTableQuestions/ && python2 evaluator.py ./results/result_4344.json')
+# Run evaluation
+subprocess.run(
+    ['python2', evaluator_script, relative_prediction_path],
+    cwd=evaluator_dir
+)
